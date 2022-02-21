1602b6000e5ca695cdec61e58adb5612a65e78bf
Гузель Насуртдинова <nasurtdinovag@mail.ru>
2022-02-21 10:10:19 +0300

    Add class catsitter

--- MobileAppGroup4/MobileAppGroup4/SQLite/Catsitter.cs
+++ MobileAppGroup4/MobileAppGroup4/SQLite/Catsitter.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MobileAppGroup4.SQLite
 {
+    [Table("Cats")]
     public class Catsitter
     {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public Boolean Housing { get; set; }
+        public Boolean PracYears { get; set; }
+        public DateTime birthdayDate { get; set; }
+        public int Phone { get; set; }
+        public string Address { get; set; }
+        public string Info { get; set; }
+        public Boolean Child { get; set; }
+        public Boolean Medicines { get; set; }
     }
 }
